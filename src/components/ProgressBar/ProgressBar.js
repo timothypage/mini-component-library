e456f7ae56d6dc8890a57e949058353dcecdc3b2
Tim Zwolak <timothypage@gmail.com>
2023-06-30 15:23:51 -0600

    ProgressBar 1.0

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,30 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  large: '24px',
+  medium: '12px',
+  small: '8px'
+}
+
+
+const Styled = styled.div`
+  height: ${props => SIZES[props.size]};
+  background: ${COLORS.transparentGray15};
+  border-radius: 4px;
+  box-shadow: 0px 2px 4px 0px #80808059 inset;
+
+  overflow: hidden;
+`;
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Styled size={size}>
+      <VisuallyHidden>progress is {value} percent</VisuallyHidden>
+      <div style={{height: 100+'%', width: value + '%', background: "blue"}}></div>
+    </Styled>
+  );
 };
+
 
 export default ProgressBar;
