c0ed71c4957424a088c67e466c7ebfec54b8bc72
Tim Zwolak <timothypage@gmail.com>
2023-07-02 16:15:42 -0600

    working select display

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,61 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+
+const StyledSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  opacity: 0;
+  appearance: none;
+  `;
+
+const StyledSelectDisplay = styled.div`
+  background: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: 1rem;
+  font-family: "Roboto", sans-serif;
+  border-radius: 8px;
+  padding: 12px 16px;
+  padding-right: 52px;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+
+  width: 24px;
+  height: 24px;
+
+  margin: auto;
+`;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <StyledSelect value={value} onChange={onChange}>
+        {children}
+      </StyledSelect>
+      <StyledSelectDisplay>
+        {displayedValue}&nbsp; 
+        <IconWrapper>
+          <Icon id="chevron-down" size={24} />
+        </IconWrapper>
+      </StyledSelectDisplay>
+    </Wrapper>
+
   );
 };
 
