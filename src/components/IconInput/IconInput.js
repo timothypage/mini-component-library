df383eaec46c8e20779f1231212786411b989c0b
Tim Zwolak <timothypage@gmail.com>
2023-07-03 11:31:54 -0600

    my attempt at IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,59 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+import { useRef } from 'react';
+
+const Wrapper = styled.div`
+  display: flex;
+  position: relative;
+
+  border-radius: 4px;
+
+  &:focus-within {
+    outline: 3px solid ${COLORS.primary};
+  }
+`;
+
+const IconWrapper = styled.div`
+  padding-right: 12px;
+
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+
+  margin: auto;
+
+  width: 24px;
+  height: 24px;
+`;
+
+const StyledInput = styled.input`
+  padding-left: 32px;
+
+  border: none;
+  border-bottom: 2px solid black;
+  outline: none;
+
+  flex-grow: 2;
+
+  font-size: var(--size);
+
+  font-family: Roboto, sans-serif;
+  color: ${COLORS.gray700};
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+
+  &:hover&::placeholder {
+    color: black;
+  }
+`;
+
+
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +66,23 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const inputRef = useRef(null);
+
+  const iconClick = () => {
+    inputRef.current.focus();
+  }
+
+  return (
+    <Wrapper style={{width}}>
+      <IconWrapper onClick={iconClick}>
+        <Icon id={icon} size={size == 'large' ? 24 : 16} />
+      </IconWrapper>
+      <VisuallyHidden>
+        <label>{label}</label>
+      </VisuallyHidden>
+      <StyledInput ref={inputRef} style={{'--size': size == 'large' ? '2rem' : '1rem'}} type="text" placeholder={placeholder} />
+    </Wrapper>
+  )
 };
 
 export default IconInput;
